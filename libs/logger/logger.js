f5072b60938ce515e4c319c60e2f0284df47dd6f
KolesnykMA <maxymura@gmail.com>
2022-05-10 12:16:24 +0300

    feat: add tests to circle config

---
+++ libs/logger/logger.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const winston_1 = require("winston");
+const logger = (0, winston_1.createLogger)({
+    levels: {
+        emerg: 0,
+        alert: 1,
+        crit: 2,
+        error: 3,
+        warning: 4,
+        notice: 5,
+        info: 6,
+        debug: 7,
+    },
+    transports: [new winston_1.transports.Console()],
+});
+exports.default = logger;
