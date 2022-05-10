d9ae36b193b531bd2062c5b6b2ba4923d4dd148e
KolesnykMA <maxymura@gmail.com>
2022-05-10 21:28:49 +0300

    fix: fix authenticate

--- src/auth/authorize/handler.ts
+++ src/auth/authorize/handler.ts
@@ -2,18 +2,21 @@ import jwt from 'jsonwebtoken';
 import type {Callback} from 'aws-lambda/handler';
 import type {APIGatewayTokenAuthorizerEvent} from 'aws-lambda/trigger/api-gateway-authorizer';
 import type {DecodedToken} from '../../../libs/types/auth';
+import logger from '../../../libs/logger/logger';
 
 const authorize = async (
   event: APIGatewayTokenAuthorizerEvent,
   callback: Callback<DecodedToken>
 ): Promise<DecodedToken | void> => {
-  const token = event.authorizationToken.split(' ')[1];
+  const token = event.authorizationToken?.split(' ')[1];
+  logger.debug(`Received token`, {token});
 
   if (!token) {
     return callback('Unauthorized', null);
   }
 
   const decodedToken = jwt.verify(token, process.env.JWT_SECRET) as DecodedToken;
+  logger.debug(`Decoded token`, {decodedToken});
 
   if (!decodedToken.accountId || !decodedToken.userId || !decodedToken.role) {
     return callback('Unauthorized', null);
