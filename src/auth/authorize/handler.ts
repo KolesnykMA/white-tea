dc88d8cce6aa67ad074a6267ea9536e700354e7e
KolesnykMA <maxymura@gmail.com>
2022-05-10 22:42:04 +0300

    feat: fix auth

--- src/auth/authorize/handler.ts
+++ src/auth/authorize/handler.ts
@@ -1,23 +1,25 @@
 import jwt from 'jsonwebtoken';
-import type {Callback} from 'aws-lambda/handler';
+import type {Callback, Context} from 'aws-lambda/handler';
 import type {APIGatewayTokenAuthorizerEvent} from 'aws-lambda/trigger/api-gateway-authorizer';
 import type {DecodedToken} from '../../../libs/types/auth';
 import logger from '../../../libs/logger/logger';
 
 const authorize = async (
   event: APIGatewayTokenAuthorizerEvent,
+  context: Context,
   callback: Callback<DecodedToken>
 ): Promise<DecodedToken | void> => {
-  logger.debug(`Received event`, {event});
+  logger.info(`Received event`, {event});
+
   const token = event.authorizationToken?.split(' ')[1];
-  logger.debug(`Received token`, {token});
+  logger.info(`Received token`, {token});
 
   if (!token) {
     return callback('Unauthorized', null);
   }
 
   const decodedToken = jwt.verify(token, process.env.JWT_SECRET) as DecodedToken;
-  logger.debug(`Decoded token`, {decodedToken});
+  logger.info(`Decoded token`, {decodedToken});
 
   if (!decodedToken.accountId || !decodedToken.userId || !decodedToken.role) {
     return callback('Unauthorized', null);
